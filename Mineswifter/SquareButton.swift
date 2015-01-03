779536469cc534292754171b1dcb626aa3147197
Jared Smith <jared.rit@gmail.com>
2015-01-03 00:25:44 -0600

    Make first pass at square button view

---
+++ Mineswifter/SquareButton.swift
@@ -0,0 +1,29 @@
+//
+//  SquareButton.swift
+//  Mineswifter
+//
+//  Created by Jared Smith on 1/3/15.
+//  Copyright (c) 2015 tutorial. All rights reserved.
+//
+
+import UIKit
+
+class SquareButton : UIButton {
+    let squareSize:CGFloat
+    let squareMargin:CGFloat
+    var square:Square
+    
+    init(squareModel:Square, squareSize:CGFloat, squareMargin:CGFloat) {
+        self.square = squareModel
+        self.squareSize = squareSize
+        self.squareMargin = squareMargin
+        let x = CGFloat(self.square.col) * (squareSize + squareMargin)
+        let y = CGFloat(self.square.row) * (squareSize + squareMargin)
+        let squareFrame = CGRectMake(x, y, squareSize, squareSize)
+        super.init(frame: squareFrame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
